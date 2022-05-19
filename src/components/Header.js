5879f64a2c2a3afbc351821bf924639e303817c9
RabeloGuedes <krepscredian@gmail.com>
2022-05-19 13:54:17 -0300

    Adiciona diretório (components), além de criar e adicionar à página Wallet o componenete Header com o email do usuário, total convertido e moeda corrente (BRL).

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <h5 data-testid="email-field">
+          { email }
+        </h5>
+        <p data-testid="total-field">
+          0
+        </p>
+        <p data-testid="header-currency-field">
+          BRL
+        </p>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
