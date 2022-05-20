fdbf351964f2dd31c5b38125c5f5e7e04538d4b6
RabeloGuedes <krepscredian@gmail.com>
2022-05-20 16:46:07 -0300

    Implementa lógica de adicao das despesas adicionadas à tela.

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -1,39 +1,97 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 export class ExpensesTable extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
-        <th>
-          Descrição
-        </th>
-        <th>
-          Tag
-        </th>
-        <th>
-          Método de pagamento
-        </th>
-        <th>
-          Valor
-        </th>
-        <th>
-          Moeda
-        </th>
-        <th>
-          Câmbio utilizado
-        </th>
-        <th>
-          Valor convertido
-        </th>
-        <th>
-          Moeda de conversão
-        </th>
-        <th>
-          Editar/Excluir
-        </th>
+        <table>
+          <tbody>
+            {
+              expenses
+                .map(
+                  (
+                    {
+                      id,
+                      value,
+                      description,
+                      currency,
+                      method,
+                      tag,
+                      exchangeRates,
+                    },
+                  ) => (
+                    <tr key={ id }>
+                      <th>
+                        Descrição
+                      </th>
+                      <td>
+                        { description }
+                      </td>
+                      <th>
+                        Tag
+                      </th>
+                      <td>
+                        { tag }
+                      </td>
+                      <th>
+                        Método de pagamento
+                      </th>
+                      <td>
+                        {method}
+                      </td>
+                      <th>
+                        Valor
+                      </th>
+                      <td>
+                        {(Math.ceil(value * 100) / 100).toFixed(2)}
+                      </td>
+                      <th>
+                        Moeda
+                      </th>
+                      <td>
+                        { exchangeRates[currency].name }
+                      </td>
+                      <th>
+                        Câmbio utilizado
+                      </th>
+                      <td>
+                        { Math.round(exchangeRates[currency].ask * 100) / 100 }
+                      </td>
+                      <th>
+                        Valor convertido
+                      </th>
+                      <td>
+                        { Math.ceil(value * exchangeRates[currency].ask * 100) / 100 }
+                      </td>
+                      <th>
+                        Moeda de conversão
+                      </th>
+                      <td>
+                        Real
+                      </td>
+                      <th>
+                        Editar/Excluir
+                      </th>
+                    </tr>
+                  ),
+                )
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
-export default ExpensesTable;
+ExpensesTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(ExpensesTable);
